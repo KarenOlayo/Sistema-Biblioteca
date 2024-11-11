5f4b2b913d10c8f69fec916ee76fce13475f7180
KarenOlayo <vergaraolayokaren@gmail.com>
2024-11-11 18:43:24 -0500

    Modificación métodos de las clases Biblioteca, Inventario y Estante

--- Estante.py
+++ Estante.py
@@ -33,3 +33,20 @@ class Estante:
     
     def set_nro_libros_estante(self):
         self.__nro_libros_estante += 1
+        
+    def buscar_libro_estante(self, titulo, codigo_isbn):
+        for indice, libro in enumerate(self.__nro_libros_estante):
+            if indice >= self.__nro_libros_estante:
+                break
+            if libro is not None:
+                if libro.get_titulo() == titulo or libro.get_codigo_isbn() == codigo_isbn:
+                    return indice, libro
+        return -1, None
+    
+    def eliminar_libro_estante(self, titulo, codigo_isbn):
+        indice, libro = self.buscar_libro_estante(titulo, codigo_isbn)
+        if indice != -1:
+            for libro in range(indice, self.__nro_libros_estante, 1):
+                self.__libros_estante[libro] = self.__libros_estante[libro+1]
+            self.__libros_estante[libro+1] = None
+            self.__nro_libros_estante -= 1
